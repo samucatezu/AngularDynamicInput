81e89b4aef56eadd19c847d6b07cbdb5f3257d78
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-06 22:03:33 -0300

    Feature added

--- src/app/pages/test-field/test-field.component.ts
+++ src/app/pages/test-field/test-field.component.ts
@@ -1,5 +1,7 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
+
 import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
@@ -10,7 +12,13 @@ export class TestFieldComponent {
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
 
-  constructor(private observer: BreakpointObserver) {}
+  empForm: FormGroup;
+
+  constructor(private observer: BreakpointObserver, private fb: FormBuilder) {
+    this.empForm = this.fb.group({
+      employees: this.fb.array([]),
+    });
+  }
 
   ngAfterViewInit() {
     this.observer.observe(['(max-width: 2000px)']).subscribe((res) => {
@@ -22,5 +30,48 @@ export class TestFieldComponent {
         this.sidenav.open();
       }
     });
+  }
+  employees(): FormArray {
+    return this.empForm.get('employees') as FormArray;
+  }
+
+  newEmployee(): FormGroup {
+    return this.fb.group({
+      firstName: '',
+      lastName: '',
+      skills: this.fb.array([]),
+    });
+  }
+
+  addEmployee() {
+    console.log('Adding a employee');
+    this.employees().push(this.newEmployee());
+  }
+
+  removeEmployee(empIndex: number) {
+    this.employees().removeAt(empIndex);
+  }
+
+  employeeSkills(empIndex: number): FormArray {
+    return this.employees().at(empIndex).get('skills') as FormArray;
+  }
+
+  newSkill(): FormGroup {
+    return this.fb.group({
+      skill: '',
+      exp: '',
+    });
+  }
+
+  addEmployeeSkill(empIndex: number) {
+    this.employeeSkills(empIndex).push(this.newSkill());
+  }
+
+  removeEmployeeSkill(empIndex: number, skillIndex: number) {
+    this.employeeSkills(empIndex).removeAt(skillIndex);
+  }
+
+  onSubmit() {
+    console.log(this.empForm.value);
   }
 }
