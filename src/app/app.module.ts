815f69c0ed564e9ab47cf9d08df946fdea09187d
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-06 21:08:13 -0300

    Angular Test Base added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
+// --------------------AddYourModules_________________________
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
 import { AppRoutingModule } from './app-routing.module';
+
+// --------------------AngularTestBase_________________________
 import { AppComponent } from './app.component';
+import { layoutComponent } from './Layout/layout.component';
+import { TestFieldComponent } from './pages/test-field/test-field.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+
+// --------------------AngularTestBase_________________________
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    layoutComponent,
+    TestFieldComponent,
+    PortfolioComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatMenuModule,
+    LayoutModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
